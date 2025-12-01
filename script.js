944c309436682b651396b82fcc524d2f90d80df2
Deepam Wagh <deepam.wagh@gmail.com>
2025-12-01 16:04:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+inputs[0].focus();
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    const value = e.target.value;
+
+    e.target.value = value.replace(/[^0-9]/g, '');
+
+    if (value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "" && index > 0) {
+        inputs[index - 1].value = "";
+        inputs[index - 1].focus();
+      } else {
+        input.value = "";
+      }
+    }
+  });
+});
